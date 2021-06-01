fd5c9d0076ddedc4007c9d80e26b6d30b9c0e765
gzarapng <zara.gabriel@aluno.ifsp.edu.br>
2021-05-31 22:15:12 -0300

    react-crud instalado; começo da implementação do crud

--- client/src/Pages/Admin/index.jsx
+++ client/src/Pages/Admin/index.jsx
@@ -2,20 +2,54 @@ import '../../Styles/Global.css';
 import NavBarAdmin from '../../Components/NavBar_admin/NavBarAdmin';
 import TabelaAdmin from '../../Components/Tabelas_Admin/TabelaAdmin';
 import {Link} from 'react-router-dom';
+import Example from '../../Components/CRUDTable/index';
 
 /*VAI SER NOSSO INDEX NA TELA DO ADMIN*/
+const teste=[
+  {
+    id:1,
+    nome: "alonso",
+    idade: 16,
+    tipo: "paciente",
+  },
+  {
+    id:2,
+    nome: "afonso",
+    idade: 17,
+    tipo: "paciente",
+  }
+
+]
 
 function Admin() {
   return (
     <div> 
       <NavBarAdmin />
       <h1>Página do Adminstrador</h1>
-
-      <div className="container-btns">
-        <Link to="cliente/admin"><button></button></Link>
-        <button></button>
+      <Example/>
+      {/* <ul className="expenses-list">
+              <li className="table-title">
+                <small>Gastos Recentes</small>
+                <br />
+                
+              </li>
+              {teste.map(({ id, nome, idade, tipo }) => (
+                <li key={id} className="list-item positive">
+                  {nome}
+                  <span
+                    className={
+                      idade === "gasto" ? "value positive" : "value negative"
+                    }
+                  >
+                    {tipo}
+                    
+                  </span>
+                </li>
+              ))}
+              
+            </ul> */}
       </div>
-    </div>
+    
   );
 }
 
