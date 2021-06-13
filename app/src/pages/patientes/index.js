ebd9cda2b32a44828aa82d270d751efa0fe81e4a
Biaginibe <49575566+Biaginibe@users.noreply.github.com>
2021-06-12 21:59:11 -0300

    teste app

---
+++ app/src/pages/patientes/index.js
@@ -0,0 +1,47 @@
+import { StatusBar } from 'expo-status-bar';
+import React, { useState, useEffect, useRef } from 'react';
+import { Text, View } from 'react-native';
+import { css } from './assets/css/style';
+import MapView from 'react-native-maps';
+import * as Location from 'expo-location';
+import * as Permissions from 'expo-permissions';
+
+export default function App() {
+	const [origin, setOrigin] = useState(null);
+	const [destination, setDestination] = useState(null);
+
+	useEffect(() => {
+		(async function () {
+			const { status, permissions } = await Permissions.askAsync(
+				Permissions.LOCATION
+			);
+			if (status === 'granted') {
+				let location = await Location.getCurrentPositionAsync({
+					enableHighAccuracy: true,
+				});
+				setOrigin({
+					latitude: location.coords.latitude,
+					longitude: location.coords.longitude,
+					latitudeDelta: 0.00922,
+					longitudeDelta: 0.00421,
+				});
+			} else {
+				throw new Error('Location permission not granted');
+			}
+		})();
+	}, []);
+
+	return (
+		<View style={css.container}>
+			<MapView
+				style={css.map}
+				initialRegion={origin}
+				showsUserLocation={true}
+        loadingEnabled={true}
+			></MapView>
+			
+
+			<StatusBar style="auto" />
+		</View>
+	);
+}
