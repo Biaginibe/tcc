1a8bed31e4facd424a5596b2643bafca1d604cf1
Biaginibe <49575566+Biaginibe@users.noreply.github.com>
2021-06-15 20:59:15 -0300

    tela com mapa sem localização do user

---
+++ sendHelp/src/css/style.js
@@ -0,0 +1,19 @@
+import { StyleSheet } from 'react-native';
+
+const css = StyleSheet.create({
+	container: {
+		flex: 1,
+		backgroundColor: '#777',
+		justifyContent: 'center',
+	},
+	map: {
+        flex: 1,
+		backgroundColor: 'black',
+	},
+	source: {
+		height: '40%',
+		backgroundColor: '#fff',
+	},
+});
+
+export { css };
