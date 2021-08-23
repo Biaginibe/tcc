a2c46d1b18c8b215cb065b609ef229c8c74b4884
gzarapng <zara.gabriel@aluno.ifsp.edu.br>
2021-08-23 19:32:19 -0300

    começo da integração da agenda

--- sendHelp/src/config/axios.js
+++ sendHelp/src/config/axios.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export const instance = axios.create({
-  baseURL: 'http://4fc0-2804-431-c7fc-5f8f-f09f-5e40-3202-da5e.ngrok.io',
+  baseURL: 'http://8f6b-2804-431-c7fc-5f8f-dd9-1fa4-a2db-2d93.ngrok.io',
 });
--- sendHelp/src/pages/patiente/perfilPsycho.js
+++ sendHelp/src/pages/patiente/perfilPsycho.js
@@ -7,7 +7,6 @@ import {
   FlatList,
   SectionList,
   ScrollView,
-  
 } from "react-native";
 import { ListItem } from "react-native-elements";
 import { css } from "../../css/style";
@@ -15,54 +14,92 @@ import { instance } from "../../config/axios";
 
 export default function ProfilePsycho(route, navigation) {
   const [perfil, setPerfil] = useState();
+  const [schedule, setSchedule] = useState();
   const { valorid } = route.route.params;
 
-  
   useEffect(() => {
     async function getData() {
       try {
-		const valorrequest = valorid;
-			console.log("VALOR DA PAGINA" + valorid);
-          console.log("VALOR DE REQUEST" + valorrequest);
-          const perfildata = await instance.get(
-            `/Psychologist/${valorrequest}}/findPsychologistsjoinUsers`
-          );
-          setPerfil(perfildata.data);
-		  
+        const valorrequest = valorid;
+        console.log("VALOR DA PAGINA" + valorid);
+        console.log("VALOR DE REQUEST" + valorrequest);
+        const perfildata = await instance.get(
+          `/Psychologist/${valorrequest}}/findPsychologistsjoinUsers`
+        );
+        setPerfil(perfildata.data);
       } catch (err) {
         console.log(err);
       }
     }
     getData();
   }, [route.route.params]);
-  
+
+  useEffect(() => {
+    async function getScheduleData() {
+      try {
+        const valorrequest = valorid;
+
+        const scheduledata = await instance.get(`/psychologist/1/findSchedule`);
+        console.log(scheduledata.data);
+        setSchedule(scheduledata.data);
+      } catch (err) {
+        console.log(err);
+      }
+    }
+    getScheduleData();
+  }, [route.route.params]);
+
   return (
     <SafeAreaView style={css.container}>
       <Text>{"PERFIL DO PSICOLOGO" + valorid}</Text>
 
       <View>
         {console.log(perfil)}
-     
 
-        <FlatList data={perfil}
-				renderItem={({item})=>(
-					<ListItem bottomDivider >
-							<ListItem.Content>
-								<ListItem.Title >{item.client.user.nome}</ListItem.Title>
-								<ListItem.Subtitle>{item.tipo}</ListItem.Subtitle>
-								<ListItem.Title>{'Idade:  '+ item.client.user.idade}</ListItem.Title>
-								<ListItem.Title>{'Abordagem: ' + item.metodologia}</ListItem.Title>
-								<ListItem.Title>{'Faixa etária: ' + item.prefFaixaEtaria}</ListItem.Title>
-								<ListItem.Title>{item.valorConsulta}</ListItem.Title>
-								<ListItem.Title>{'Tempo de Sessão: ' + item.tempoSessao}</ListItem.Title>
-								<ListItem.Title>Horarios Disponiveis(ainda não sei como integrar agenda :P)</ListItem.Title>
-								<ListItem.Title><TouchableOpacity style={css.contactbutton}><Text>Entrar em Contato</Text></TouchableOpacity></ListItem.Title>
-								<ListItem.Title>{item.descricao}</ListItem.Title>
-							</ListItem.Content>
-							
-						</ListItem>
-				)}
-				/>
+        <FlatList
+          data={perfil}
+          renderItem={({ item }) => (
+            <ListItem bottomDivider>
+              <ListItem.Content>
+                <ListItem.Title>{item.client.user.nome}</ListItem.Title>
+                <ListItem.Subtitle>{item.tipo}</ListItem.Subtitle>
+                <ListItem.Title>
+                  {"Idade:  " + item.client.user.idade}
+                </ListItem.Title>
+                <ListItem.Title>
+                  {"Abordagem: " + item.metodologia}
+                </ListItem.Title>
+                <ListItem.Title>
+                  {"Faixa etária: " + item.prefFaixaEtaria}
+                </ListItem.Title>
+                <ListItem.Title>{item.valorConsulta}</ListItem.Title>
+                <ListItem.Title>
+                  {"Tempo de Sessão: " + item.tempoSessao}
+                </ListItem.Title>
+                <ListItem.Title>
+                  <TouchableOpacity style={css.contactbutton}>
+                    <Text>Entrar em Contato</Text>
+                  </TouchableOpacity>
+                </ListItem.Title>
+                <ListItem.Title>{item.descricao}</ListItem.Title>
+              </ListItem.Content>
+            </ListItem>
+          )}
+        />
+        <FlatList
+          data={schedule}
+          horizontal={true}
+          keyExtractor={(item) => String(item.id)}
+          renderItem={({ item }) => (
+            <ListItem bottomDivider>
+              <ListItem.Content>
+                <ListItem.Title>{item.diaDisponivel}</ListItem.Title>
+                <ListItem.Title>{item.horarioDisponivel}</ListItem.Title>
+                <ListItem.Title>{item.id}</ListItem.Title>
+              </ListItem.Content>
+            </ListItem>
+          )}
+        />
       </View>
     </SafeAreaView>
   );
