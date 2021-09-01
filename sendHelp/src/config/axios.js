67cc815aab3ff795185b1f8171522bf82b2135ca
gzarapng <zara.gabriel@aluno.ifsp.edu.br>
2021-08-31 21:42:38 -0300

    css perfil do marker 90%

--- sendHelp/src/config/axios.js
+++ sendHelp/src/config/axios.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export const instance = axios.create({
-  baseURL: 'http://1686-2804-431-c7fd-d403-b46e-8cc-c493-eb6b.ngrok.io',
+  baseURL: 'http://47c3-2804-431-c7fd-d403-dd9-1fa4-a2db-2d93.ngrok.io',
 });
