5d954fe4069ecdcee1f95f4c5fa257ce3d1cc114
gzarapng <zara.gabriel@aluno.ifsp.edu.br>
2021-09-15 04:07:59 -0300

    editar paciente concluido

--- sendHelp/src/config/axios.js
+++ sendHelp/src/config/axios.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export const instance = axios.create({
-  baseURL: 'http://09c8-2804-431-c7fd-78fc-903e-4f55-6b5-fed1.ngrok.io',
+  baseURL: 'http://21a1-201-27-213-107.ngrok.io',
 });
