11706c41a67c283238564bbfcbbb11f25f9d1b55
gzarapng <zara.gabriel@aluno.ifsp.edu.br>
2021-11-24 19:43:01 -0300

    axios

--- client/src/Config/axios.js
+++ client/src/Config/axios.js
@@ -2,4 +2,8 @@ import axios from 'axios';
 
 export const instance = axios.create({
   baseURL: 'http://d561-177-33-141-152.ngrok.io',
+  headers: {
+    'Access-Control-Allow-Origin': '*',
+    'Content-Type': 'application/json',
+  },
 });
